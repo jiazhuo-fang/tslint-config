4eb5afd79d2ee18b9b881d79ecfd444e7c095192
FangJiazhuo <jiazhuo.fang@ele.me>
2018-09-28 20:13:42 +0800

    添加tslint

---
+++ rules.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "await-promise": true,
+  "no-var-keyword": true,
+  "switch-default": true,
+  "curly": [true, "ignore-same-line"],
+  "no-duplicate-imports": true,
+  "no-duplicate-super": true,
+  "no-duplicate-switch-case": true,
+  "no-eval": true,
+  "no-duplicate-variable": true,
+  "no-empty": [true, "allow-empty-catch"],
+  "no-invalid-template-strings": true,
+  "no-implicit-dependencies": true,
+  "trailing-comma": [true, {
+    "multiline": "never",
+    "singleline": "never"
+  }],
+  "arrow-parens": [true, "ban-single-arg-parens"],
+  "quotemark": [true, "single"],
+  "semicolon": true,
+  "indent": [true, "spaces", 2],
+  "arrow-return-shorthand": true,
+  "encoding": true,
+  "no-consecutive-blank-lines": true,
+  "no-trailing-whitespace": true,
+  "space-before-function-paren": false,
+  "variable-name": [true, "ban-keywords", "check-format", "allow-pascal-case"]
+}
